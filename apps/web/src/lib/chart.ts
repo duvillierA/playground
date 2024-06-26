3b8f5e2103983e81dde5d7715c3c1e2b64e0b7e4
Duvillier Antoine <duvillierantoine@gmail.com>
2024-06-26 15:00:29 +0200

    fix chartData generation

--- apps/web/src/lib/chart.ts
+++ apps/web/src/lib/chart.ts
@@ -1,24 +1,20 @@
-import { random } from '@/lib/math'
 import { addDays, format } from 'date-fns'
 
+import { random } from '@/lib/math'
+
+const luminosity = (count: number, min: number, max: number) => ((count - min) / (max - min)) * (40 - 90) + 90
+
+const min = 5
+const max = 100
 export const generateChartData = (startDate: string, numEntries: number) => {
-  return [...Array(numEntries).keys()]
-    .map((i) => {
-      const date = addDays(startDate, i)
-      const day = format(date, 'yyyy-MM-dd')
-      const count = random(5, 100)
-      return {
-        day,
-        countg 
-      }
-    })
-    .sort((a, b) => {
-      return b.count - b.count
-    })
-    .map((props, i) => {
-      return {
-        ...props,
-        countColor: `hsl(213, 70%, ${80 - i * 2}%)`
-      }
-    })
+  return [...Array(numEntries).keys()].map((i) => {
+    const date = addDays(startDate, i)
+    const day = format(date, 'yyyy-MM-dd')
+    const count = random(min, max)
+    return {
+      day,
+      count,
+      countColor: `hsl(213, 70%, ${luminosity(count, min, max)}%)`
+    }
+  })
 }
